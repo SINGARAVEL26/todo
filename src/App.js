adfb9a61143a4a153bfedc2a6b50b931aaf49803
SINGARAVEL M <109954462+SINGARAVEL26@users.noreply.github.com>
2023-03-13 12:13:21 +0530

    c

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = () => {
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
